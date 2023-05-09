82784a8a8879a5f9a53c9049332a93cc042e0a29
Ram Dettmer <ramdettmer751@gmail.com>
2023-05-09 12:50:11 -0700

    change env

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-09'
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
+  useCdn: false,
 })
