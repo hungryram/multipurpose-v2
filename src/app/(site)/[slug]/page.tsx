7bf0f08fe49bbf086dfdce40796e76f56a722ee1
Ram Dettmer <ramdettmer751@gmail.com>
2023-05-10 23:16:07 -0700

    correct 404

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { notFound } from "next/navigation";
 import { getPage } from "../../../../lib/groq-data";
 import Main from "../components/templates/main";
@@ -19,34 +18,32 @@ export default async function Page({ params }: Props) {
 
     const page = await getPage(slug)
 
-    if(!page) {
+    if (!page) {
         notFound()
     }
 
     return (
-        <>
-            <Main
-                pageBuilder={page?.pageBuilder}
-                // CONTACT
-                email={page?.profileSettings?.contact_information?.email}
-                phone_number={page?.profileSettings?.contact_information?.phone_number}
-                office_number={page?.profileSettings?.contact_information?.office_number}
-                address={page?.profileSettings?.address?.address}
-                city={page?.profileSettings?.address?.city}
-                state={page?.profileSettings?.address?.state}
-                zip_code={page?.profileSettings?.address?.zip_code}
+        <Main
+            pageBuilder={page?.pageBuilder}
+            // CONTACT
+            email={page?.profileSettings?.contact_information?.email}
+            phone_number={page?.profileSettings?.contact_information?.phone_number}
+            office_number={page?.profileSettings?.contact_information?.office_number}
+            address={page?.profileSettings?.address?.address}
+            city={page?.profileSettings?.address?.city}
+            state={page?.profileSettings?.address?.state}
+            zip_code={page?.profileSettings?.address?.zip_code}
 
-                // FORMS
-                emailAlerts={page?.profileSettings?.settings?.emailAlerts}
-                sendFrom={page?.profileSettings?.settings?.sendFrom}
-                emailBcc={page?.profileSettings?.settings?.emailBcc}
-                emailCc={page?.profileSettings?.settings?.emailCc}
-                // PAGE FOLDERS
-                allServices={page?.allServices}
-                allTestimonials={page?.allTestimonial}
-                allBlog={page?.allBlog}
-                allTeam={page?.allTeam}
-            />
-        </>
+            // FORMS
+            emailAlerts={page?.profileSettings?.settings?.emailAlerts}
+            sendFrom={page?.profileSettings?.settings?.sendFrom}
+            emailBcc={page?.profileSettings?.settings?.emailBcc}
+            emailCc={page?.profileSettings?.settings?.emailCc}
+            // PAGE FOLDERS
+            allServices={page?.allServices}
+            allTestimonials={page?.allTestimonial}
+            allBlog={page?.allBlog}
+            allTeam={page?.allTeam}
+        />
     )
 }
