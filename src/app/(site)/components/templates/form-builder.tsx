e980fe4bfc0b0427c843bba5b8050bf76d6509ed
adz-brad <bradley.smith@arcticleaf.io>
2023-05-15 16:19:46 -0600

    add submitForm server action to form builder

--- src/app/(site)/components/templates/form-builder.tsx
+++ src/app/(site)/components/templates/form-builder.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ServerClient } from 'postmark';
+import { submitForm } from './_formActions'
 
 interface FormField {
   name: string;
@@ -17,38 +17,11 @@ interface FormBuilderProps {
 }
 
 export default function FormBuilder({ formSchema }: FormBuilderProps) {
-  const serverToken: string = process.env.NEXT_PUBLIC_POSTMARK_API_TOKEN || '';
-  const client = new ServerClient(serverToken);
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
-    // Submit the form data to your backend or perform other actions
-    try {
-      // Send the form data to your backend API
-      const response = client.sendEmail({}) //PLEASE ADD YOUR EMAIL CODE HERE
-
-
-      if (response.ok) {
-        // Form submission successful
-        console.log('Form submitted successfully');
-        // Perform any additional actions or show success message
-      } else {
-        // Form submission failed
-        console.error('Form submission failed');
-        // Perform error handling or show error message
-      }
-    } catch (error) {
-      console.error('An error occurred while submitting the form', error);
-      // Perform error handling or show error message
-    }
-  };
 
   return (
     <div className="py-10 px-4">
       <h2>{formSchema?.title}</h2>
-      <form onSubmit={onSubmit}>
+      <form action={submitForm}>
         {formSchema?.fields && (
           <>
             {formSchema.fields.map((field) => {
