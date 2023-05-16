0b2d30b18c21e81443531193faab57fc4149e407
Ram Dettmer <ramdettmer751@gmail.com>
2023-05-15 23:18:54 -0700

    formatted form

--- src/app/(site)/components/templates/_formActions.js
+++ src/app/(site)/components/templates/_formActions.js
@@ -5,18 +5,37 @@ import { ServerClient } from 'postmark'
 const client = new ServerClient(process.env.NEXT_PUBLIC_POSTMARK_API_TOKEN);
 
 export const submitForm = async (data) => {
-    let formData = {}
-    for( let [name] of data.entries()){
-        if(!name.includes('$ACTION_ID')){
-            formData[name] = data.get(name) 
-        }
+  let formData = {}
+  for (let [name] of data.entries()) {
+    if (!name.includes('$ACTION_ID')) {
+      formData[name] = data.get(name)
     }
-    client.sendEmail({
-        "From": '<from_email>', // must match sender signature on postmark account
-        "To": "<to_email>",
-        "Subject": "<subject>",
-        "TextBody": JSON.stringify(formData),
-    })
+  }
+  
+  const tableRows = Object.entries(formData).map(([key, value]) => {
+    return `
+      <tr>
+        <td>${key}</td>
+        <td>${value}</td>
+      </tr>
+    `;
+  });
+
+  const htmlBody = `
+    <h1>Contact Form Submission</h1>
+    <table>
+      <tbody>
+        ${tableRows.join('')}
+      </tbody>
+    </table>
+  `;
+  
+  client.sendEmail({
+    "From": 'forms@hungryramwebdesign.com', // must match sender signature on postmark account
+    "To": "ram@hungryram.com",
+    "Subject": "Inquiry",
+    "HtmlBody": htmlBody,
+  })
     .then((res) => console.log(res))
     .catch((err) => console.error(err))
-  }
+}
